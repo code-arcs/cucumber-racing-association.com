ed211c3c1c628e63ccc98b7dc5e2c60fc22f6dcf
Stephan Köninger <stephan.koeninger@gbtec.de>
2016-09-30 08:46:30 +0200

    chore(cucumber): le cucumber calculation has been moved to server - did someone say WS rock?

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+'use strict';
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const Cucumber = require('./cucumber');
+
 
 io.on('connection', function(client) {
     console.log('Client connected...');
 
     client.on('join', function(data) {
-        console.log(data);
+        for(let i = 0; i < Cucumber._getRandomInt(5,15); i++) {
+            const x = Cucumber._getRandomInt(50, data.windowWidth - 50) / data.windowWidth;
+            client.emit('throwCucumber', Cucumber.create(x, Cucumber._getRandomInt(20, 100)));
+        }
     });
 
     client.on('throwCucumber', function(data) {
-        console.log(data);
-        io.emit('throwCucumber', data);
+        io.emit('throwCucumber', Cucumber.create(data.x, data.y));
     });
 });
 
--- public/js/cucumber.js
+++ public/js/cucumber.js
@@ -2,42 +2,34 @@
 (function () {
 
     var socket = io();
-    socket.on('connect', function (data) {
-        socket.emit('join', 'Hello World from client');
+    socket.on('connect', function () {
+        socket.emit('join', {windowWidth: window.innerWidth});
     });
 
     socket.on('throwCucumber', throwCucumberToWindow);
 
-    var minWidthOfCucumber = 35;
-    var maxWidthOfCucumber = 55;
-
     document.body.addEventListener('click', function (event) {
-        socket.emit('throwCucumber', {x: event.clientX, y: event.clientY, now: true});
+        socket.emit('throwCucumber', {x: event.clientX  / window.innerWidth, y: event.clientY});
     }, false);
 
     document.body.addEventListener("touchstart", function (event) {
-        throwCucumberToWindow({x: event.touches[0].clientX, y: event.touches[0].clientY, now: true});
+        socket.emit('throwCucumber', {x: event.touches[0].clientX, y: event.touches[0].clientY});
     }, false);
 
-    for (var i = 0; i < getRandomInt(5, 15); i++) {
-        var x = getRandomInt(maxWidthOfCucumber + 5, window.innerWidth - (maxWidthOfCucumber + 5));
-        var y = getRandomInt(10, 120);
-        throwCucumberToWindow({x: x, y: y});
-    }
-
     function throwCucumberToWindow(data) {
-        const x = data.x;
-        const y = data.y;
-        const now = data.now;
-
-        var width = getRandomInt(minWidthOfCucumber, maxWidthOfCucumber);
-        var animationDuration = getRandomInt(25, 50);
+        var width = data.width;
+        var padLeft = (width / window.innerWidth)/2;
+        var y = data.y;
+        var x = (data.x - padLeft) * 100;
+        var animationDuration = data.animationDuration;
+        var rotation = data.rotation;
+        var bezier = data.bezier;
 
         var cucumberWrap = document.createElement('div');
-        cucumberWrap.className = now ? 'cucumber-wrap' : 'cucumber-wrap delay-1s';
+        cucumberWrap.className = 'cucumber-wrap';
         cucumberWrap.style.animationDuration = animationDuration + 's';
-        cucumberWrap.style.animationTimingFunction = getBezier();
-        cucumberWrap.style.left = x - (width / 2) + 'px';
+        cucumberWrap.style.animationTimingFunction = bezier;
+        cucumberWrap.style.left = x + '%';
         cucumberWrap.style.top = y - (width / 2) + 'px';
         cucumberWrap.style.width = width + 'px';
         cucumberWrap.style.height = width + 'px';
@@ -45,33 +37,12 @@
         var cucumber = document.createElement('img');
         cucumber.src = 'img/cucumber.png';
         cucumber.className = 'cucumber noselect';
-        cucumber.style.transform = 'rotate(' + getRandomInt(0, 360) + 'deg)';
+        cucumber.style.transform = 'rotate(' + rotation + 'deg)';
         cucumber.style.width = width + 'px';
         cucumber.style.bottom = '-' + width / 2 + 'px';
 
         cucumberWrap.appendChild(cucumber);
 
         document.querySelector('.wrapper').appendChild(cucumberWrap);
-    }
-
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    function getBezier() {
-        var bez = [
-            getRandomFloat(0.2, 0.8),
-            getRandomFloat(0.2, 0.5),
-            getRandomFloat(0.2, 0.5),
-            getRandomFloat(0.2, 0.5)
-        ].join(', ');
-
-        return 'cubic-bezier(' + bez + ')';
-
-        function getRandomFloat(min, max) {
-            return (Math.random() * (max - min)) + min;
-        }
     }
 })();
