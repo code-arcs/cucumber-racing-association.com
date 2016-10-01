e1c40c7b37a1a538eba12dc108e3c42095027d74
Stephan Köninger <stephan.koeninger@gbtec.de>
2016-10-01 17:26:38 +0200

    chore(fadeout): fades out cucumbers when they have reached bottom

--- public/js/cucumber.js
+++ public/js/cucumber.js
@@ -10,7 +10,7 @@
     socket.on('info', refreshUI);
 
     document.body.addEventListener('click', function (event) {
-        socket.emit('throwCucumber', {x: event.clientX  / window.innerWidth, y: event.clientY});
+        socket.emit('throwCucumber', {x: event.clientX / window.innerWidth, y: event.clientY});
         refreshCounterThrownByUser();
     }, false);
 
@@ -19,23 +19,25 @@
         refreshCounterThrownByUser();
     }, false);
 
+
     function throwCucumberToWindow(data) {
         var width = data.width;
-        var padLeft = (width / window.innerWidth)/2;
+        var padLeft = (width / window.innerWidth) / 2;
         var y = data.y;
         var x = (data.x - padLeft) * 100;
         var animationDuration = data.animationDuration;
         var rotation = data.rotation;
         var bezier = data.bezier;
+        var top = y - (width / 2);
 
         var cucumberWrap = document.createElement('div');
         cucumberWrap.className = 'cucumber-wrap';
         cucumberWrap.style.animationDuration = animationDuration + 's';
         cucumberWrap.style.animationTimingFunction = bezier;
         cucumberWrap.style.left = x + '%';
-        cucumberWrap.style.top = y - (width / 2) + 'px';
+        cucumberWrap.style.top = top + 'px';
         cucumberWrap.style.width = width + 'px';
-        cucumberWrap.style.height = width + 'px';
+        cucumberWrap.style.height = window.innerHeight - top + 'px';
 
         var cucumber = document.createElement('img');
         cucumber.src = 'img/cucumber.png';
@@ -43,10 +45,28 @@
         cucumber.style.transform = 'rotate(' + rotation + 'deg)';
         cucumber.style.width = width + 'px';
         cucumber.style.bottom = '-' + width / 2 + 'px';
+        cucumber.ondragstart = function () {
+            return false;
+        };
 
         cucumberWrap.appendChild(cucumber);
-
         document.querySelector('.wrapper').appendChild(cucumberWrap);
+
+        setTimeout(function() { fade(cucumber)}, animationDuration * 1000);
+        setTimeout(function() { fade(cucumberWrap)}, animationDuration * 1000 * 1.5);
+    }
+
+    function fade(element) {
+        var op = 1;  // initial opacity
+        var timer = setInterval(function () {
+            if (op <= 0.1){
+                clearInterval(timer);
+                element.parentNode.removeChild(element);
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op -= op * 0.1;
+        }, 50);
     }
 
     function refreshUI(data) {
